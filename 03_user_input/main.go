9b652529e9b1b4bfe17093016c7281d79e2ab536
Hamed Yarandi <hamed.yarandi@gmail.com>
2023-05-04 21:21:26 +0330

    User input in Go

---
+++ 03_user_input/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+
+	welcome := " "
+	fmt.Println(welcome)
+
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("Enter the rating for our product:")
+ 
+	//comma ok || err ok
+	input, _ := reader.ReadString('\n')
+
+	fmt.Println("Thanks for rating, ", input)
+	fmt.Printf("Type of rating is %T", input)
+
+}
