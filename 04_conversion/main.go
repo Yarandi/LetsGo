6a539ff39a9f528cc8534b7cd2e20c1e4afeae1b
Hamed Yarandi <hamed.yarandi@gmail.com>
2023-05-04 20:07:08 +0330

    conversion

---
+++ 04_conversion/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main()  {
+
+	fmt.Println("Welcome to our pizza app")
+	fmt.Println("Please rate our pizza between 1 and 5")
+
+	reader := bufio.NewReader(os.Stdin)
+	input, _ := reader.ReadString('\n')
+
+	fmt.Println("Yourr rate is: ", input)
+	convertedInput, err := strconv.ParseFloat(strings.TrimSpace(input), 64)
+
+	if convertedInput > 5 || convertedInput < 1 {
+		fmt.Println("you should rate between 1 to 5")
+		return
+	}else {
+	    fmt.Println(convertedInput)
+	}
+
+	if err != nil {
+		fmt.Println(err)
+	}else {
+		fmt.Println("your rate is: ", convertedInput)
+	}
+	
+}
