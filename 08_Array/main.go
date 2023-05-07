6ea384daf4510eb4f1639fe62fc3e75d4469cbd5
Hamed Yarandi <hamed.yarandi@gmail.com>
2023-05-07 07:46:17 +0330

    arrays in go

---
+++ 08_Array/main.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func main(){
+
+	fmt.Println("WElcome to array in golang")
+
+	// var fruitList [4]string
+
+	// fruitList[0] = "benana"
+	// fruitList[1] = "cherry"
+	// fruitList[3] = "apple"
+
+	// fmt.Println("this is FruitList:", fruitList)
+	// fmt.Println("lenght of FruitList is:", len(fruitList))
+
+
+	//another type of definition array
+	// var vegList = [5]string {"potato","beans","mushroom"}
+	// fmt.Println(vegList)
+	// fmt.Println(len(vegList))
+
+	//another type of definition array
+	var arrList3 = [5]int {1,2,3,4}
+	fmt.Println(arrList3)
+	fmt.Println(len(arrList3))
+}
